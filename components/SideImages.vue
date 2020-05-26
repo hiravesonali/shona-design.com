6f638c9953a74ab705778727f1c6868afe6e69a9
Sonali Hirave <hiravesonali@gmail.com>
2020-05-26 21:31:00 +0200

    Created layout components

---
+++ components/SideImages.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full">
+    <div class="relative h-full max-w-screen-xl mx-auto">
+      <svg
+        class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
+        width="404"
+        height="784"
+        fill="none"
+        viewBox="0 0 404 784"
+      >
+        <defs>
+          <pattern
+            id="svg-pattern-squares-1"
+            x="0"
+            y="0"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+          >
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="784" fill="url(#svg-pattern-squares-1)" />
+      </svg>
+      <svg
+        class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+        width="404"
+        height="784"
+        fill="none"
+        viewBox="0 0 404 784"
+      >
+        <defs>
+          <pattern
+            id="svg-pattern-squares-2"
+            x="0"
+            y="0"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+          >
+            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+          </pattern>
+        </defs>
+        <rect width="404" height="784" fill="url(#svg-pattern-squares-2)" />
+      </svg>
+    </div>
+  </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,77 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        About shona-design.com
-      </h1>
-      <h2 class="subtitle">
-        About my personal website!
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <main-header />
+    <div class="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div class="max-w-7xl mx-auto">
+        <div class="text-center">
+          <h2
+            class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10"
+          >100 Days of Design</h2>
+          <p class="mt-3 max-w-2xl mx-auto text-xl leading-7 text-gray-500 sm:mt-4">
+            I'm doing
+            <a
+              href="https://twitter.com/hashtag/100DaysOfDesign?src=hashtag_click"
+              target="_blank"
+              class="text-indigo-600"
+            >#100DaysOfDesign</a> challenge to boost my skills.
+            <br />I use
+            <a href="https://procreate.art" target="_blank" class="text-indigo-600">ProCreate</a> on my iPad to create these illustrations.
+            <br />Send me the positive vibes.🙌
+          </p>
+        </div>
+
+        <div class="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
+          <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+            <a
+              href="https://shona-design.com/designs/mountains-and-lake"
+              title="Mountains and Lake"
+            >
+              <div class="flex-shrink-0">
+                <img
+                  class="h-auto w-full object-cover"
+                  src="https://shona-design.com/storage/vAXMvzzjGuZfxzB68HdHUUnE4Yu42rFJFEGbKaWz.jpeg"
+                  alt="Mountains and Lake"
+                />
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
