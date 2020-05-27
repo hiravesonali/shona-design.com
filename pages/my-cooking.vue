a0483a443a4780d9999f7546651084c4691ca915
Sonali Hirave <hiravesonali@gmail.com>
2020-05-27 14:09:14 +0200

    my cooking page

---
+++ pages/my-cooking.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div class="max-w-3xl mx-auto">
+        <div>
+          <h1
+            class="text-3xl leading-9 tracking-tight font-medium text-gray-900 sm:text-4xl sm:leading-10"
+          >Cooking is an expression that crosses boundaries.</h1>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >When I'm not designing(🍥) or painting(🎨) or sleeping(😴), you can definitely find me trying different cuisines in my kitchen.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >Have a look at some of hand cooked dishes in past two years:</p>
+
+          <p class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4">
+            <img src="~/assets/images/food.png" />
+          </p>
+        </div>
+      </div>
+    </div>
+    <about-me-section />
+    <hobbies />
+  </div>
+</template>
