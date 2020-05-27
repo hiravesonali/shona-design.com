68915e0ad2ee57c471b06a167da3a0d41082b91f
Sonali Hirave <hiravesonali@gmail.com>
2020-05-27 14:01:40 +0200

    about page done

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
-    About
+    <div class="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div class="max-w-3xl mx-auto">
+        <div>
+          <h1
+            class="text-3xl leading-9 tracking-tight font-medium text-gray-900 sm:text-4xl sm:leading-10"
+          >A little bit about myself.</h1>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >I'm a product designer &amp; ProCreator based in Nuremberg, Germany with experience in delivering end-to-end UX/UI design for software products and brands.</p>
+
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >I've worked for different business domains like Banking, E-learning, B2B SaaS and B2C Couponing platform.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >In my past experience I've worked on logo design, brand design, corporate identity kit, web and mobile application design</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >I'm passionate about improving the lives of others through design and I am constantly looking to learn new things everyday.</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >Feel free to contact me and have a great day!🙌</p>
+        </div>
+      </div>
+    </div>
+    <about-me-section />
+    <hobbies />
   </div>
 </template>
-<script>
-export default {
-}
-</script>
+
