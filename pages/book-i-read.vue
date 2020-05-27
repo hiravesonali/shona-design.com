067a891a93b49108b10e26329c14a3a883771cc8
Sonali Hirave <hiravesonali@gmail.com>
2020-05-27 14:22:17 +0200

    I read book page

---
+++ pages/book-i-read.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div class="max-w-3xl mx-auto">
+        <div>
+          <h1
+            class="text-3xl leading-9 tracking-tight font-medium text-gray-900 sm:text-4xl sm:leading-10"
+          >For me reading is the way to get lost at something.</h1>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >I'm not a vivid reader but I do like Novels, Fairy Tale and History books(📙).</p>
+          <p class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4">
+            It's hard to remember content but I do remember the storyline in the end 😛.
+            Below are some books which I remember I've finished reading at some point in past.
+          </p>
+
+          <p class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4">
+            <img src="~/assets/images/book.jpeg" />
+          </p>
+        </div>
+      </div>
+    </div>
+    <about-me-section />
+    <hobbies />
+  </div>
+</template>
---
+++ pages/my-paintings.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div class="max-w-3xl mx-auto">
+        <div>
+          <h1
+            class="text-3xl leading-9 tracking-tight font-medium text-gray-900 sm:text-4xl sm:leading-10"
+          >Painting is a way to express your feelings.</h1>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >I take brush and canvas occasionally to express my heart & feelings in the form of paintings(🎨).</p>
+          <p
+            class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
+          >Here are some shots from my previous paintings, not so many and also I'm not professional painter 😛.</p>
+
+          <p class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4">
+            <img src="~/assets/images/painting.jpg" />
+          </p>
+        </div>
+      </div>
+    </div>
+    <about-me-section />
+    <hobbies />
+  </div>
+</template>
