586626ee144efbe87a61512151aa5238c1c7fb70
Sonali Hirave <hiravesonali@gmail.com>
2020-05-27 19:14:08 +0200

    tweet add in 100 days of code page

--- pages/100-days-of-code.vue
+++ pages/100-days-of-code.vue
@@ -9,6 +9,27 @@
           <p
             class="mt-3 text-xl leading-7 text-gray-500 sm:mt-4"
           >To make learning as a fun I've started doing 100 days of code from April-2020.</p>
+          <blockquote class="twitter-tweet">
+            <p lang="en" dir="ltr">
+              Day 10 of the challenge
+              <a
+                href="https://twitter.com/hashtag/100DaysOfCode?src=hash&amp;ref_src=twsrc%5Etfw"
+              >#100DaysOfCode</a>
+              <br />Today, I&#39;ve learned php composer, how I can use it. Also, I&#39;ve made a simple working backend for twitter application. No backend framework, pure PHP &amp; Tailwindcss so far.
+              <br />[10/100]😺
+              <a
+                href="https://twitter.com/hashtag/WomenWhoCode?src=hash&amp;ref_src=twsrc%5Etfw"
+              >#WomenWhoCode</a>
+              <a
+                href="https://twitter.com/hashtag/CodeNewbie?src=hash&amp;ref_src=twsrc%5Etfw"
+              >#CodeNewbie</a>
+              <a href="https://t.co/aIIYO464gf">pic.twitter.com/aIIYO464gf</a>
+            </p>&mdash; Sonali Hirave (@ShonaDesign)
+            <a
+              href="https://twitter.com/ShonaDesign/status/1260913733687279616?ref_src=twsrc%5Etfw"
+            >May 14, 2020</a>
+          </blockquote>
+          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
       </div>
     </div>
