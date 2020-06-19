a01764150878963de30110cddfd1d1d04f257a00
Sonali Hirave <hiravesonali@gmail.com>
2020-06-19 18:59:12 +0200

    changed overflow to auto in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-200 text-body font-body overflow-scroll">
+  <div class="bg-gray-200 text-body font-body overflow-auto">
     <div class="relative bg-gray-50 overflow-hidden">
       <side-images />
       <div class="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
